6b37fcac0210761983a223b0ec8a9cca10ff49c3
Tuwi-0808374 <0808374@hr.nl>
2025-04-11 10:51:19 +0200

    api call ophalen post

--- flask_server/blueprints/post/models/post_model.py
+++ flask_server/blueprints/post/models/post_model.py
@@ -1,8 +1,10 @@
-import sqlite3
+import sqlite3, os
 
 class Post:
     def __init__(self):
-        self.cursor, self.con = ('./databases/database.db').connect_db()
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.path = os.path.join(base_dir, '../../../databases/database.db') 
+        self.cursor, self.con = self.connect_db()
 
     def connect_db(self):
         con = sqlite3.connect(self.path)
@@ -14,4 +16,5 @@ class Post:
         query = "SELECT * FROM posts"
         self.cursor.execute(query)
         posts = self.cursor.fetchall()
-        return posts
+        result_dicts = [dict(row) for row in posts]
+        return result_dicts
--- flask_server/blueprints/post/post.py
+++ flask_server/blueprints/post/post.py
@@ -5,10 +5,17 @@ from blueprints.post.models.post_model import Post
 post_bp = Blueprint('post', __name__)
 base = Blueprint('base', __name__)
 
-@post_bp.route('/posts', methods=['GET'])
+@post_bp.route('/example', methods=['GET'])
 def get_examples():
     data = [
         {'id': 1, 'name': 'Voorbeeld 1'},
         {'id': 2, 'name': 'Voorbeeld 2'}
     ]
     return jsonify({'status': 'success', 'data': data})
+
+@post_bp.route('/posts', methods=['GET'])
+def get_posts():
+    post = Post()
+    posts = post.get_posts()
+    return jsonify({'status': 'success', 'data': posts})
+
