30b934a46967869e60eccbaa23180bfad98d2221
JesperEdeling-1103541 <your.email@example.com>
2025-04-19 16:19:30 +0200

    WIP krijgt de tags tezien (op pc) bij het maken van een post maar krijgt ze niet te zien op android

--- react_client/app/CreatePost.tsx
+++ react_client/app/CreatePost.tsx
@@ -1,8 +1,34 @@
-const COLORS = {
-    red: '#C80032',
-    background: '#F8F4EF',
-    text: '#333333',
-    textLight: '#FFFFFF',
-    inputLine: '#555555',
-    placeholderText: '#666666',
-};
+import React, { useState, useEffect } from "react";
+import { View, Text } from 'react-native';
+
+
+//https://www.youtube.com/watch?v=7LNl2JlZKHA
+export default function CreatePost() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/tags")
+            .then(res => res.json())
+            .then(data => {
+                setData(data.data);
+                console.log(data.data);
+            })
+    }, []);
+
+    return (
+        <View style={{ padding: 20 }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Tags:</Text>
+            {data.map((tag, i) => (
+                <Text key={i}>
+                    {tag['title']}
+                    {'\n'}
+                    {tag['content']}
+                    {'\n'}
+                </Text>
+
+            ))}
+        </View>
+    );
+}
+
