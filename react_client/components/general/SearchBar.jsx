13b7f0c603ad0be8d417d2a732bd85e493fe6cb1
Max-1103166 <1103166@hr.nl>
2025-04-21 14:28:56 +0200

    created searchbar.jsx

---
+++ react_client/components/general/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { TextInput, View } from 'react-native';
+
+function SearchBar() {
+
+    return (
+        <View>
+            <TextInput
+                placeholder="Search tags"
+                // value={}
+                // onChangeText={}
+            />
+        </View>
+    );
+}
+
+export default SearchBar;
