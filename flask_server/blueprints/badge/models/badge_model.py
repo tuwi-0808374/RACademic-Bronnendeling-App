4dc63690850ac80595819816e4caf6722b5e3df6
Tuwi-0808374 <0808374@hr.nl>
2025-05-06 18:39:39 +0200

    added give a badge to user functionality

--- flask_server/blueprints/badge/models/badge_model.py
+++ flask_server/blueprints/badge/models/badge_model.py
@@ -21,3 +21,26 @@ class Badge:
         badges = self.cursor.fetchall()
         result_dicts = [dict(row) for row in badges]
         return result_dicts
+    
+    def has_user_badge(self, user_id, badge_id):
+        self.cursor.execute('''
+            SELECT * FROM user_badges 
+            WHERE user_id = ? AND badge_id = ?''', (user_id, badge_id))
+        result = self.cursor.fetchone()
+        if result:
+            return True
+        return False
+
+    
+    def give_badge_to_user(self, user_id, badge_id):
+        message = "Badge already given to user"
+        if self.has_user_badge(user_id, badge_id):
+            return False, message
+        
+        self.cursor.execute('''
+            INSERT INTO user_badges (user_id, badge_id) 
+            VALUES (?, ?)''', (user_id, badge_id))
+        self.con.commit()
+        
+        message = "Badge given to user"
+        return True, message
