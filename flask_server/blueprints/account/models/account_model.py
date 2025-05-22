dea19469cf2f255478fc802bd907d3a5895ac605
Noortje-1100338 <1100338@hr.nl>
2025-05-22 15:41:49 +0200

    Check if user is banned in login

--- flask_server/blueprints/account/models/account_model.py
+++ flask_server/blueprints/account/models/account_model.py
@@ -31,7 +31,8 @@ class Account:
                 password,
                 last_name,
                 first_name || ' ' || last_name AS full_name,
-                username
+                username,
+                is_banned
             
                 FROM users
                 WHERE email = ?
@@ -47,6 +48,7 @@ class Account:
                     "hashed_password": result[3],
                     "full_name": result[5],
                     "username": result[6],
+                    "is_banned": result[7],
                     
                     
                 }
