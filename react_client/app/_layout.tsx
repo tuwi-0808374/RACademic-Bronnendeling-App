ebd065466761f1fb5f74329f0ede37b926ec3117
Max-1103166 <1103166@hr.nl>
2025-05-25 01:40:06 +0200

    wip bugfix mobile. pages dont always show

--- react_client/app/_layout.tsx
+++ react_client/app/_layout.tsx
@@ -1,79 +1,43 @@
 import { Stack } from 'expo-router';
 import { useState } from "react";
-import {ScrollView, StyleSheet, TouchableWithoutFeedback, View} from "react-native";
+import { ScrollView, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 import NavBar from "@/components/general/NavBar";
 import TagContainer from "@/components/general/TagContainer";
 import { getApiBaseUrl } from '@/constants/get_ip';
 
-export default function RootLayout() {
+export default function Layout() {
   const [visible, setVisible] = useState(false);
   const [selectedTags, setSelectedTags] = useState({});
   const API_BASE_URL = getApiBaseUrl();
 
-  const handleInsidePress = () => {
-    setVisible(true);
-  }
-  const handleClose = () => {
-    setVisible(false);
-  }
+  const handleInsidePress = () => setVisible(true);
+  const handleClose = () => setVisible(false);
 
   return (
+      <View style={styles.pageContainer}>
+        <View style={styles.navbarContainer}>
+          <NavBar
+              setVisible={setVisible}
+              handleInsidePress={handleInsidePress}
+              selectedTags={selectedTags}
+              API_BASE_URL={API_BASE_URL}
+          />
+          <TagContainer
+              visible={visible}
+              selectedTags={selectedTags}
+              setSelectedTags={setSelectedTags}
+          />
+        </View>
 
-  <View style={styles.pageContainer}>
-
-    <View style={styles.navbarContainer} >
-        <NavBar
-        setVisible={setVisible}
-        handleInsidePress={handleInsidePress}
-        selectedTags={selectedTags}
-        API_BASE_URL={API_BASE_URL}
-        />
-
-      <TagContainer
-      visible={visible}
-      selectedTags={selectedTags}
-      setSelectedTags={setSelectedTags}
-      />
-    </View>
-
-    <TouchableWithoutFeedback onPress={handleClose}>
-      <ScrollView style={styles.contentContainer}>
-        <Stack>
-          <Stack.Screen
-              name='account/profile'
-              options={{headerShown:false,}}
-          />
-          <Stack.Screen
-              name="index"
-              options={{headerShown: false,}}
-          />
-          <Stack.Screen
-              name="posts"
-              options={{headerShown: false,}}
-          />
-          <Stack.Screen
-            name="posts/list_favorite"
-            options={{headerShown:false,}}
-          />
-          <Stack.Screen
-              name="posts/most_upvoted"
-              options={{headerShown:false,}}
-          />
-          <Stack.Screen
-              name="posts/create_post"
-              options={{headerShown:false,}}
-          />
-          <Stack.Screen
-              name="posts/edit_post"
-              options={{headerShown:false,}}
-          />
-
-        </Stack>
-      </ScrollView>
-    </TouchableWithoutFeedback>
-  </View>
+        <TouchableWithoutFeedback onPress={handleClose}>
+          <View style={styles.contentContainer}>
+            <Stack screenOptions={{ headerShown: false }} />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   pageContainer:{
